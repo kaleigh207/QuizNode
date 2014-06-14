6d18293156fe983cdc94a9d8a953990eb3e4aaed
Taylor1791 <Taylor@1791.com>
2014-06-13 19:47:30 -0500

    Testing hsot

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+var static = require('node-static');
 var http = require('http');
 
 var db = require('./db/index.js');
@@ -5,11 +6,16 @@ var db = require('./db/index.js');
 db.ready(function() {
   var Quiz = db.Quiz;
 
-  http.createServer(function(req, res) {
+  var clientApp = new static.Server('./client');
+  var serverApp = function(req, res) {
     Quiz.find({}, function(err, quizes) {
       if(err) return res.end(err);
       return res.end(JSON.stringify(quizes));
     });
+  };
+
+  http.createServer(function(req, res) {
+    res.end(req.headers.host);
   }).listen(8080);
 
 });
